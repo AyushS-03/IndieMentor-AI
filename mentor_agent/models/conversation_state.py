4fcb37fd7a02914ac27eb14767b19ff8e6541b8c
Madhurjya Dasgupta <dasguptamadhurjya@gmail.com>
2025-07-01 01:37:05 +0530

    Fixed bug related to Mentor model

--- mentor_agent/models/conversation_state.py
+++ mentor_agent/models/conversation_state.py
@@ -4,3 +4,6 @@ from pydantic import BaseModel
 class MentorState(BaseModel):
     user_id: str
     input: str
+    profile: dict = {}
+    reply: str = ""  # Add this field
+    analytics: dict = {}  # Add this field
